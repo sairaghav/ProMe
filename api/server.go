81219c7e6d1643b3e2961eaef8369461752ce388
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-10-20 05:35:16 +0200

    Added basic directions API

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var apiKey string = "RErLEHOyAx6DKiFoJeJv8uDAkLe9LnGR"
+var baseUrl string = "http://open.mapquestapi.com"
+
+func getDirections(from string, to string) string {
+	fmt.Println(from, to)
+	finalUrl := baseUrl + "/directions/v2/route?key=" + apiKey + "&from=" + from + "&to=" + to
+	response, _ := http.Get(finalUrl)
+
+	responseData, _ := ioutil.ReadAll(response.Body)
+	return string(responseData)
+}
+
+func main() {
+	http.HandleFunc("/directions/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, getDirections(strings.Split(r.URL.Path, "/")[2], strings.Split(r.URL.Path, "/")[3]))
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+}
