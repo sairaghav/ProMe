5e91d80b653b2808d04ca54c45686434c0809135
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-10-23 23:22:31 +0200

    Added tags for death and investigations

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-trackingTags = ['furti','droga','arresti','incidenti','rapine','vandali','incidenti-stradali','aggressioni']
+trackingTags = ['furti','droga','arresti','incidenti','rapine','vandali','incidenti-stradali','aggressioni','indagini','morti']
 
 mapBaseUrl = "http://open.mapquestapi.com"
 mapApiKey = os.environ['MAPS_API_KEY']
