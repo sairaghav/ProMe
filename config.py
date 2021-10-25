27f16c8b46af67bc6bf06dbadcd6a86ba9898a8c
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-10-25 06:20:12 +0200

    Added comments

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-trackingTags = ['furti','droga','arresti','incidenti','rapine','vandali','incidenti-stradali','aggressioni','indagini','morti']
+trackingTags = ['furti','droga','arresti','incidenti','rapine','vandali','incidenti-stradali','aggressioni','indagini','morti'] #Will hold exhaustive collection of all tags to check
 
 mapBaseUrl = "http://open.mapquestapi.com"
 mapApiKey = os.environ['MAPS_API_KEY']
 
-newsSource = {"MilanoToday": "https://www.milanotoday.it"}
+newsSource = {"MilanoToday": "https://www.milanotoday.it"} #Other news sources can be added in the same format
 
