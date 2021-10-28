800578b23cf67b0165403302f93de5e2e37d76f4
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-10-28 18:22:40 +0200

    Added scripts and readme

--- scripts/generateNewsDataset.py
+++ scripts/generateNewsDataset.py
@@ -31,13 +31,13 @@ endDate = datetime.datetime.now().strftime("%Y-%m-%d")
 startDate = "2018-01-01"#(datetime.datetime.now() - datetime.timedelta(days = 730)).strftime("%Y-%m-%d")
 
 with open('../datasets/NewsData_'+startDate+'_'+endDate+'.csv', 'w+', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Date","Time","Source","Street","Tags","Link"])
     for street in streets:
         finalUrl = baseUrl+"/"+street.replace(' ','%20')+"/"+startDate+"/"+endDate
         print(finalUrl)
         response = requests.get(finalUrl)
         responseData = json.loads(response.text)
 
-        writer = csv.writer(f)
-        writer.writerow(["Date","Time","Source","Street","Tags","Link"])
         for data in responseData[street]:
             writer.writerow([data['date'],data['time'],data['source'],data['street'],data['tags'],data['link']])
