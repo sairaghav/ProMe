2df00cf8bc3943076057ea862d63adcccfae11c0
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-11-14 23:40:13 +0100

    Fixed error for news data retrieval

--- ProMeAPI/views.py
+++ ProMeAPI/views.py
@@ -33,19 +33,54 @@ def get_news(request) -> JsonResponse:
     street = request.GET.get('street',None)
     from_date = request.GET.get('from',(datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=config.fetch_news_for_interval_days)).strftime('%Y-%m-%d'))
     to_date = request.GET.get('to',datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d'))
+    
+    # Avoid future dates
+    if datetime.datetime.strptime(to_date, '%Y-%m-%d').astimezone(datetime.timezone.utc) <= datetime.datetime.now(datetime.timezone.utc):
+        to_date = to_date
+    else:
+        to_date = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d')
 
     if street is not None:
+        # Check if data for street is already evaluated
+        queryset = StreetList.objects.all()
+        queryset = queryset.filter(street=street)
+
+        # Add the dates that were checked for news for the street to the DB
+        if len(queryset) == 0:
+            street_list = StreetList(street=street,news_from=from_date,news_till=to_date)
+            street_list.save()
+            queryset = StreetList.objects.all()
+
+        available_from = datetime.datetime.strptime(queryset.filter(street=street).values('news_from')[0]['news_from'], '%Y-%m-%d').astimezone(datetime.timezone.utc)
+        available_till = datetime.datetime.strptime(queryset.filter(street=street).values('news_till')[0]['news_till'], '%Y-%m-%d').astimezone(datetime.timezone.utc)
+
         from_date = datetime.datetime.strptime(from_date, '%Y-%m-%d').astimezone(datetime.timezone.utc)
         to_date = datetime.datetime.strptime(to_date, '%Y-%m-%d').astimezone(datetime.timezone.utc)
 
         queryset = StreetRisk.objects.all()
+        
+        # Check news articles for only the dates that were not analysed already
+        street_list = StreetList.objects.get(street=street)
+        if from_date < available_from:
+            if to_date > available_till:
+                add_to_db(street, queryset, from_date.strftime('%Y-%m-%d'), to_date.strftime('%Y-%m-%d'))
+                street_list.news_from = from_date.strftime('%Y-%m-%d')
+                street_list.news_till = to_date.strftime('%Y-%m-%d')
+                street_list.save(update_fields=['news_from', 'news_till'])
+            else:
+                add_to_db(street, queryset, from_date.strftime('%Y-%m-%d'), available_till.strftime('%Y-%m-%d'))
+                street_list.news_from = from_date.strftime('%Y-%m-%d')
+                street_list.save(update_fields=['news_from'])
+
+        else:
+            if to_date > available_till:
+                add_to_db(street, queryset, available_till.strftime('%Y-%m-%d'), to_date.strftime('%Y-%m-%d'))
+                street_list.news_till = to_date.strftime('%Y-%m-%d')
+                street_list.save(update_fields=['news_till'])
+        
+        
+        queryset = StreetRisk.objects.all()
         queryset = queryset.filter(street=street,date__range=[from_date,to_date])
-
-        if len(queryset) == 0:
-            results = add_to_db(street, queryset, from_date.strftime('%Y-%m-%d'), to_date.strftime('%Y-%m-%d'))
-
-            queryset = StreetRisk.objects.all()
-            queryset = queryset.filter(street=street,date__range=[from_date,to_date])
 
         response = Response(results=list(queryset.values()), errors=None)
 
