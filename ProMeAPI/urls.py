1f0c35b7e8884e066f0abd7e301400f0993407e1
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-11-23 11:04:36 +0100

    Removed dead code

--- ProMeAPI/urls.py
+++ ProMeAPI/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path, include
-from rest_framework import routers
-
 from . import views
 
-router = routers.DefaultRouter()
-#router.register(r'news', views.StreetRiskViewSet, basename='streetrisk')
-
 urlpatterns = [
-    #path('', views.index, name='index'),
-    path('', include(router.urls)),
     path(r'auth/', include('djoser.urls')),
     path(r'auth/', include('djoser.urls.authtoken')),
     path(r'news', views.get_news_for_street, name='news'),
