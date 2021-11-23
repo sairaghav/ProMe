d447dae978868ef5eed3cb22e97eaac19cad3215
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-11-23 01:28:26 +0100

    Checkboxes for tags in report

--- ProMeWeb/reportform.py
+++ ProMeWeb/reportform.py
@@ -1,7 +1,17 @@
 from django import forms
 from ProMeAPI.models import StreetRisk
+from ProMe.config import trackingTags
+
+tag_list = []
+
+for tag in trackingTags:
+    tag_list.append((tag.lower(),tag.capitalize()))
 
 class StreetReportForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['tags'].widget = forms.CheckboxSelectMultiple(choices=tag_list)
     class Meta:
         model = StreetRisk
         fields = ['street', 'news','tags']
