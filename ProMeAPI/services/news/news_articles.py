fef05314d393633d2540cf0fc6746c81cea9311a
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-11-24 22:56:06 +0100

    Moved config file and refactored code

--- ProMeAPI/services/news/news_articles.py
+++ ProMeAPI/services/news/news_articles.py
@@ -95,17 +95,20 @@ def add_user_reported_incidents(street: str, summary: str, tags: str, user='Test
 
 # Convert all time values to UTC
 def get_utc_from_to_date(from_date: str, to_date: str) -> tuple[(datetime.datetime,datetime.datetime)]:
-    from_date = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=config.fetch_news_for_interval_days+2)).strftime('%Y-%m-%d') if from_date is None else from_date
+    from_date = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=config.fetch_news_for_interval_days+1)).strftime('%Y-%m-%d') if from_date is None else from_date
     to_date = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=1)).strftime('%Y-%m-%d') if to_date is None else to_date
+    
     # Avoid future dates
     if datetime.datetime.strptime(to_date, '%Y-%m-%d').astimezone(datetime.timezone.utc) <= datetime.datetime.now(datetime.timezone.utc):
         to_date = datetime.datetime.strptime(to_date, '%Y-%m-%d').astimezone(datetime.timezone.utc)
+        from_date = to_date - datetime.timedelta(days=config.fetch_news_for_interval_days)
     else:
         to_date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=1)
-    
+        from_date = to_date - datetime.timedelta(days=config.fetch_news_for_interval_days)
+        
     # Avoid from_date greater than to_date
     if to_date < datetime.datetime.strptime(from_date, '%Y-%m-%d').astimezone(datetime.timezone.utc):
-        from_date = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=config.fetch_news_for_interval_days))
+        from_date = (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=config.fetch_news_for_interval_days+1))
     else:
         from_date = datetime.datetime.strptime(from_date, '%Y-%m-%d').astimezone(datetime.timezone.utc)
 
