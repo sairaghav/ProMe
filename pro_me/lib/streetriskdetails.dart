d5e94767150cd9ce16beddac5215dfbdff4390a5
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-11-30 18:25:49 +0100

    Added risk table for risk details

--- pro_me/lib/streetriskdetails.dart
+++ pro_me/lib/streetriskdetails.dart
@@ -15,10 +15,71 @@ class _StreetRiskDetailsState extends State<StreetRiskDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const ProMeAppBar(),
-      body: Column(
-        children: <Widget>[
-          Flexible(child: Text('${widget.details['risk_score']}')),
-        ],
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Safety Information for ' + widget.details["street"],
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(
+                      label: Text(
+                        'Date',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Source',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Tags',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Summary',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Link',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ],
+                  rows: widget.details['risk_metadata']
+                      .map<DataRow>(
+                        ((element) => DataRow(
+                              cells: <DataCell>[
+                                DataCell(Text(element['date'])),
+                                DataCell(Text(element['source'])),
+                                DataCell(Text(element['tags'])),
+                                DataCell(Text(element['news'])),
+                                DataCell(Text(element['link'])),
+                              ],
+                            )),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
+            Expanded(child: Text('${widget.details}')),
+          ],
+        ),
       ),
       bottomNavigationBar: const ProMeNavBar(
         selectedIndex: 1,
